5f111d323bde0137a74bf38a56c1864a538042d0
Valentine <38808380+Jayyaah@users.noreply.github.com>
2026-01-21 11:18:48 +0100

    Fetch API

---
+++ js/api/api.js
@@ -0,0 +1,18 @@
+import { API_KEY } from '../config.js';
+const BASE_URL = 'https://api.rawg.io/api';
+
+/**
+ * Fetch data from the RAWG Video Games Database API.
+ * @param {string} endpoint - The API endpoint to fetch data from.
+ * @param {Object} params - An object containing query parameters.
+ */
+async function fetchFromAPI(endpoint, params = {}) {
+  const url = new URL(`${BASE_URL}${endpoint}`);
+  url.searchParams.append('key', API_KEY);
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`API request failed with status ${response.status}`);
+    }
+    return response.json();
+} 
