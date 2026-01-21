b3fef820156613e353e5222865ca4324eac669ec
Valentine <38808380+Jayyaah@users.noreply.github.com>
2026-01-21 14:48:53 +0100

    Update storage.js

---
+++ js/utils/storage.js
@@ -0,0 +1,21 @@
+const STORAGE_KEY = 'gametracker_favorites';
+
+export function getFavorites() {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+}
+
+export function isFavorite(gameId) {
+    return getFavorites().some(game => game.id === gameId);
+}
+
+export function toggleFavorite(game) {
+    const favorites = getFavorites();
+    const gameIndex = favorites.findIndex(fav => fav.id === game.id);
+
+    if (index === -1) {
+        favorites.push(game);
+    } else {    
+        favorites.splice(gameIndex, 1);
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(favorites));
+}
