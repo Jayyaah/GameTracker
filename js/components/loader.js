69bacab0c8bcad91afda7f433c038fe56019ba00
Valentine <38808380+Jayyaah@users.noreply.github.com>
2026-01-21 11:56:33 +0100

    Update loader.js

---
+++ js/components/loader.js
@@ -0,0 +1,10 @@
+export function createLoader() {
+  const loader = document.createElement('div');
+  loader.className = 'loader';
+  loader.innerHTML = `
+    <div class="spinner"></div>
+    <p>Chargement...</p>
+  `;
+  return loader;
+}
+
