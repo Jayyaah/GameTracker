5074a59df8c997a384e0b258f5cd4b59ab43c47f
Valentine <38808380+Jayyaah@users.noreply.github.com>
2026-01-21 14:13:52 +0100

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,27 +1,17 @@
 import { homeView } from "./views/homeView.js";
-import { gameView } from "./views/gameView.js";
-import { favoritesView } from "./views/favoritesView.js";
-import { searchView } from "./views/searchView.js";
 
 const routes = {
   "/": homeView,
-  "/game": gameView,
-  "/favorites": favoritesView,
-  "/search": searchView,
 };
 
 export function router() {
   const app = document.getElementById("app");
+  if (!app) return;
 
-  const hash = location.hash.slice(1) || "/";
-  const [path, param] = hash.split("/");
-
-  const view = routes[path];
-
-  if (!view) {
-    app.innerHTML = "<h2>Page introuvable</h2>";
+  if (!location.hash || location.hash === "#/" || location.hash === "#") {
+    routes["/"]();
     return;
   }
 
-  view(param);
+  app.innerHTML = "<h2>Page introuvable</h2>";
 }
