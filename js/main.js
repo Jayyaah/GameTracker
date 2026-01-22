5ae83600dfe011491c5335809d281a3d987bbf3f
Valentine <38808380+Jayyaah@users.noreply.github.com>
2026-01-22 12:08:59 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 import { router } from "./router.js";
+import { searchView } from "./views/searchView.js";
 
 window.addEventListener("hashchange", router);
 window.addEventListener("load", router);
+
+const input = document.getElementById("searchInput");
+input.addEventListener("input", (event) => {
+    searchView(event.target.value);
+});
