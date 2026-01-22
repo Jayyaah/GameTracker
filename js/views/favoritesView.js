13a027733d814aaaa4eeb4a10e2818e550d2402e
Valentine <38808380+Jayyaah@users.noreply.github.com>
2026-01-22 13:26:46 +0100

    Update favoritesView.js

---
+++ js/views/favoritesView.js
@@ -0,0 +1,15 @@
+import { createGameGrid } from "../components/gameGrid";
+import { getFavorites } from "../utils/storage";
+
+export function favoritesView() {
+    const app = document.getElementById('app');
+    const favorites = getFavorites();
+
+    app.innerHTML = '<h2>Mes Jeux Favoris</h2>';
+    if (favorites.length === 0) {
+        app.innerHTML += '<p>Tu n\'as pas encore ajouté de jeux favoris.</p>';
+        return;
+    } if (favorites.length > 0) {
+        app.appendChild(createGameGrid(favorites));
+    }
+}
