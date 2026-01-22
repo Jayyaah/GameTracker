f942398894d0d436152881c4ed0290d5ba9ce396
Valentine <38808380+Jayyaah@users.noreply.github.com>
2026-01-22 13:26:29 +0100

    add debounce

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 import { router } from "./router.js";
+import { debounce } from "./utils/debounce.js";
 import { searchView } from "./views/searchView.js";
 
 window.addEventListener("hashchange", router);
 window.addEventListener("load", router);
 
+const debouncedSearchView = debounce(searchView, 300);
 const input = document.getElementById("searchInput");
+
 input.addEventListener("input", (event) => {
-    searchView(event.target.value);
+  const query = event.target.value;
+  debouncedSearchView(query);
 });
