45693cbea8e6976b10eef9c18e52281831b9fb90
Valentine <38808380+Jayyaah@users.noreply.github.com>
2026-01-22 12:06:30 +0100

    Update searchView.js

--- js/views/searchView.js
+++ js/views/searchView.js
@@ -1,5 +1,5 @@
 import {
-    searchGames } from '../api/api.js';
+    searchGames } from '../api/rawgApi.js';
 
 import { createLoader } from '../components/loader.js';
 import { createErrorMessage } from '../components/errorMessage.js';
@@ -7,19 +7,29 @@ import { createGameGrid } from '../components/gameGrid.js';
 
 export async function searchView(query) {
     const app = document.getElementById('app');
+    if (query.length < 2) {
+        return;
+    }
     app.innerHTML = '';
     const loader = createLoader();
     app.appendChild(loader);
     try {
-        const results = await searchGames(query);
+        const data = await searchGames(query);
+        const results = data.results;
+        
+        if (results.length === 0) {
+            app.innerHTML = `<h2>Aucun résultat trouvé pour "${query}"</h2>`;
+            return;
+        }
+
         app.innerHTML = `<h2>Résultats de la recherche pour "${query}"</h2>`;
         app.appendChild(createGameGrid(results));
     } catch (error) {
         app.innerHTML = '';
         app.appendChild(
-            createErrorMessage(
-                'Impossible de charger les résultats de la recherche. Vérifie ta connexion.'
+        createErrorMessage(
+            'Impossible de charger les résultats de la recherche. Vérifie ta connexion.'
             )
         );
-    }
+    } 
 }
