ec7fdef3a497466e990595f52f31139c8aaf34bc
Valentine <38808380+Jayyaah@users.noreply.github.com>
2026-02-04 16:42:46 +0100

    Update genreSelect.js

--- js/components/filters/genreSelect.js
+++ js/components/filters/genreSelect.js
@@ -11,7 +11,7 @@ export function genreSelect(genres, onChange) {
 
   genres.forEach(g => {
     const option = document.createElement('option');
-    option.value = g.name; // IMPORTANT : on filtre par name
+    option.value = g.name;
     option.textContent = g.name;
     selectElement.appendChild(option);
   });
